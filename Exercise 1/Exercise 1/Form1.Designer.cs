cfb21b352742412906d92bc2cf78c3f5307ee4d3
Chase Hausman <chase.hausman@comcast.net>
2018-11-03 16:52:11 -0700

    Update to use Visual C# instead of Visual Basic.

---
+++ Exercise 1/Exercise 1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Exercise_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excitingBtn = new System.Windows.Forms.Button();
+            this.boringBtn = new System.Windows.Forms.Button();
+            this.checkBoxRed = new System.Windows.Forms.CheckBox();
+            this.checkBoxGreen = new System.Windows.Forms.CheckBox();
+            this.checkBoxBlue = new System.Windows.Forms.CheckBox();
+            this.checkBoxWhite = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // excitingBtn
+            // 
+            this.excitingBtn.Location = new System.Drawing.Point(87, 250);
+            this.excitingBtn.Name = "excitingBtn";
+            this.excitingBtn.Size = new System.Drawing.Size(148, 23);
+            this.excitingBtn.TabIndex = 0;
+            this.excitingBtn.Text = "Do Something Exciting";
+            this.excitingBtn.UseVisualStyleBackColor = true;
+            this.excitingBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // boringBtn
+            // 
+            this.boringBtn.Location = new System.Drawing.Point(87, 288);
+            this.boringBtn.Name = "boringBtn";
+            this.boringBtn.Size = new System.Drawing.Size(148, 23);
+            this.boringBtn.TabIndex = 0;
+            this.boringBtn.Text = "Do Something Boring";
+            this.boringBtn.UseVisualStyleBackColor = true;
+            this.boringBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBoxRed
+            // 
+            this.checkBoxRed.AutoSize = true;
+            this.checkBoxRed.Location = new System.Drawing.Point(123, 114);
+            this.checkBoxRed.Name = "checkBoxRed";
+            this.checkBoxRed.Size = new System.Drawing.Size(46, 17);
+            this.checkBoxRed.TabIndex = 1;
+            this.checkBoxRed.Text = "Red";
+            this.checkBoxRed.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGreen
+            // 
+            this.checkBoxGreen.AutoSize = true;
+            this.checkBoxGreen.Location = new System.Drawing.Point(123, 137);
+            this.checkBoxGreen.Name = "checkBoxGreen";
+            this.checkBoxGreen.Size = new System.Drawing.Size(55, 17);
+            this.checkBoxGreen.TabIndex = 1;
+            this.checkBoxGreen.Text = "Green";
+            this.checkBoxGreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBlue
+            // 
+            this.checkBoxBlue.AutoSize = true;
+            this.checkBoxBlue.Location = new System.Drawing.Point(123, 160);
+            this.checkBoxBlue.Name = "checkBoxBlue";
+            this.checkBoxBlue.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxBlue.TabIndex = 1;
+            this.checkBoxBlue.Text = "Blue";
+            this.checkBoxBlue.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxWhite
+            // 
+            this.checkBoxWhite.AutoSize = true;
+            this.checkBoxWhite.Location = new System.Drawing.Point(123, 183);
+            this.checkBoxWhite.Name = "checkBoxWhite";
+            this.checkBoxWhite.Size = new System.Drawing.Size(54, 17);
+            this.checkBoxWhite.TabIndex = 1;
+            this.checkBoxWhite.Text = "White";
+            this.checkBoxWhite.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What Colors are Okay?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "What is your name?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 403);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxWhite);
+            this.Controls.Add(this.checkBoxBlue);
+            this.Controls.Add(this.checkBoxGreen);
+            this.Controls.Add(this.checkBoxRed);
+            this.Controls.Add(this.boringBtn);
+            this.Controls.Add(this.excitingBtn);
+            this.Name = "Form1";
+            this.Text = "Simple Questions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button excitingBtn;
+        private System.Windows.Forms.Button boringBtn;
+        private System.Windows.Forms.CheckBox checkBoxRed;
+        private System.Windows.Forms.CheckBox checkBoxGreen;
+        private System.Windows.Forms.CheckBox checkBoxBlue;
+        private System.Windows.Forms.CheckBox checkBoxWhite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
